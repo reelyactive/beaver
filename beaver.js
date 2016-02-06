29ac607660afb674f5729ef145673477be8c7d3f
jeffyactive <jeff@reelyactive.com>
2016-02-06 15:03:29 -0500

    Fetch associations from API

--- beaver.js
+++ beaver.js
@@ -3,13 +3,19 @@
  * We believe in an open Internet of Things
  */
 
+
+DEFAULT_ASSOCIATIONS_API_URL = 'http://www.hyperlocalcontext.com/associations/';
+
+
 angular.module('reelyactive.beaver', [])
 
   .factory('beaver', function beaverFactory($http) {
 
     var devices = {};
+    var associations = {};
     var stats = { appearances: 0, displacements: 0, keepalives: 0,
                   disappearances: 0 };
+    var associationsApiUrl = DEFAULT_ASSOCIATIONS_API_URL;
 
     function updateDevice(event, data) {
       if(!data || !data.tiraid) {
@@ -39,13 +45,35 @@ angular.module('reelyactive.beaver', [])
       }
 
       var device = devices[id];
-      if(!device.hasOwnProperty('url')) {
-        // TODO: fetch association
+      if(!device.hasOwnProperty('associations')) {
+        getAssociations(id, function(id, associations) {
+          if(devices.hasOwnProperty(id)) {
+            devices[id].associations = associations;
+          }
+        });
       }
 
       if(event === 'appearance') { stats.appearances++; }
       if(event === 'displacement') { stats.displacements++; }
       if(event === 'keep-alive') { stats.keepalives++; }
+    }
+
+    function getAssociations(id, callback) {
+      if(associations.hasOwnProperty(id)) {
+        return callback(id, associations[id]);
+      }
+      associations[id] = {};
+      $http.defaults.headers.common.Accept = 'application/json';
+      $http.get(associationsApiUrl + id)
+        .success(function(data, status, headers, config) {
+          if(data.devices && data.devices[id]) {
+            associations[id] = data.devices[id];
+          }
+          return callback(id, associations[id]);
+        })
+        .error(function(data, status, headers, config) {
+          return callback(id, associations[id]);
+        });
     }
 
     var handleSocketEvents = function(Socket) {
