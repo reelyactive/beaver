bf85c56072a11668ca17d3d81a912a0ae7ea9a7e
jeffyactive <jeff@reelyactive.com>
2017-07-20 17:55:49 -0400

    Added directory sampling, updated for new beaver.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,8 +8,14 @@
 DEFAULT_SOCKET_URL = 'https://www.hyperlocalcontext.com/';
 DEFAULT_POLLING_URL = 'https://www.hyperlocalcontext.com/contextat/directory/notman';
 DEFAULT_POLLING_MILLISECONDS = 5000;
-DEFAULT_ASSOCIATIONS_URL = 'https://www.hyperlocalcontext.com/associations/';
-EVENT_HISTORY = 4;
+DEFAULT_BEAVER_OPTIONS = {
+  disappearanceMilliseconds: 20000,
+  mergeEvents: false,
+  maintainDirectories: true
+};
+DEFAULT_DIRECTORY_ID = 'Unspecified';
+DEFAULT_SAMPLE_PERIOD = 2000;
+EVENT_HISTORY = 10;
 
 
 /**
@@ -25,32 +31,27 @@ angular.module('dashboard', ['reelyactive.beaver'])
  * DashCtrl Controller
  * Handles the manipulation of all variables accessed by the HTML view.
  */
-.controller('DashCtrl', function($scope, beaver) {
+.controller('DashCtrl', function($scope, $interval, beaver) {
   var socket = io.connect(DEFAULT_SOCKET_URL);
+  var directories = beaver.getDirectories();
 
   // Variables accessible in the HTML scope
   $scope.devices = beaver.getDevices();
   $scope.stats = beaver.getStats();
+  $scope.directories = [];
   $scope.events = [];
 
   // beaver.js can listen on the websockets OR poll for events
   //   (unlikely you'd want to do both, but shown here as an example)
-  beaver.listen(socket, { disappearanceMilliseconds: 20000 });
-  beaver.poll(DEFAULT_POLLING_URL, DEFAULT_POLLING_MILLISECONDS);
+  beaver.listen(socket, DEFAULT_BEAVER_OPTIONS);
+  beaver.poll(DEFAULT_POLLING_URL, DEFAULT_POLLING_MILLISECONDS,
+              DEFAULT_BEAVER_OPTIONS);
 
   // Handle events pre-processed by beaver.js
-  beaver.on('appearance', function(event) {
-    handleEvent(event);
-  });
-  beaver.on('displacement', function(event) {
-    handleEvent(event);
-  });
-  beaver.on('keep-alive', function(event) {
-    handleEvent(event);
-  });
-  beaver.on('disappearance', function(event) {
-    handleEvent(event);
-  });
+  beaver.on('appearance', handleEvent);
+  beaver.on('displacement', handleEvent);
+  beaver.on('keep-alive', handleEvent);
+  beaver.on('disappearance', handleEvent);
 
   // Handle an event
   function handleEvent(event) {
@@ -60,4 +61,25 @@ angular.module('dashboard', ['reelyactive.beaver'])
     }
   }
 
+  // Sample the current state of the directories
+  function sampleDirectories() {
+    var directoryArray = [];
+
+    for(id in directories) {
+      var directory = directories[id];
+      if(id !== 'null') {
+        directory.id = id;
+      }
+      else {    
+        directory.id = DEFAULT_DIRECTORY_ID;
+      } 
+      directory.receiverCount = Object.keys(directory.receivers).length;
+      directory.deviceCount = Object.keys(directory.devices).length;
+      directoryArray.push(directory);
+    }
+
+    $scope.directories = directoryArray;
+  }
+
+  $interval(sampleDirectories, DEFAULT_SAMPLE_PERIOD);
 });
