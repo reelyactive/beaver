a45ad58f05978a9dc45bf39a5823e726052cf6e0
jeffyactive <jeff@reelyactive.com>
2023-02-26 09:32:53 -0500

    Implemented connect icon

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ const DEFAULT_URL = 'http://pareto.local';
 
 
 // DOM elements
+let connectIcon = document.querySelector('#connectIcon');
 let paretoUrl = document.querySelector('#paretoUrl');
 let paretoButton = document.querySelector('#paretoButton');
 let statsDisplay = document.querySelector('#statsDisplay');
@@ -24,11 +25,19 @@ paretoUrl.value = DEFAULT_URL;
 // Handle beaver events
 beaver.on('connect', handleConnect);
 beaver.on('stats', handleStats);
+beaver.on('disconnect', handleDisconnect);
 
 
 // Handle stream connection
 function handleConnect() {
   statsDisplay.hidden = false;
+  connectIcon.replaceChildren(createElement('i', 'fas fa-cloud text-success'));
+}
+
+// Handle stream disconnection
+function handleDisconnect() {
+  statsDisplay.hidden = true;
+  connectIcon.replaceChildren(createElement('i', 'fas fa-cloud text-warning'));
 }
 
 // Handle stats
@@ -39,6 +48,33 @@ function handleStats(stats) {
   spatemRate.textContent = stats.eventsPerSecond.spatem.toFixed(1);
 }
 
+// Create an element as specified
+function createElement(elementName, classNames, content) {
+  let element = document.createElement(elementName);
+
+  if(classNames) {
+    element.setAttribute('class', classNames);
+  }
+
+  if((content instanceof Element) || (content instanceof DocumentFragment)) {
+    element.appendChild(content);
+  }
+  else if(Array.isArray(content)) {
+    content.forEach((item) => {
+      if((item instanceof Element) || (item instanceof DocumentFragment)) {
+        element.appendChild(item);
+      }
+      else {
+        element.appendChild(document.createTextNode(item));
+      }
+    });
+  }
+  else if(content) {
+    element.appendChild(document.createTextNode(content));
+  }
+
+  return element;
+}
 
 // Handle Pareto button click
 paretoButton.addEventListener('click', () => {
